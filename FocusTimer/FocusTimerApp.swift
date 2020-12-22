64348a62511c0d265f92c12a0912d47c81121c3d
ghigbie <gehigbie@gmail.com>
2020-12-21 22:37:43 -0500

    Initial commit

---
+++ FocusTimer/FocusTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FocusTimerApp.swift
+//  FocusTimer
+//
+//  Created by George Higbie on 12/21/20.
+//
+
+import SwiftUI
+
+@main
+struct FocusTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
