f3dd579f7852ea4bed13e08ede6dce1a9876f3b5
ghigbie <gehigbie@gmail.com>
2020-12-21 22:47:19 -0500

    Add basic elements

---
+++ FocusTimer/TimeBar.swift
@@ -0,0 +1,16 @@
+//
+//  TimeBar.swift
+//  FocusTimer
+//
+//  Created by George Higbie on 12/21/20.
+//
+
+import SwiftUI
+
+struct TimeBar: View {
+    var body: some View {
+        Text("Time Bar")
+    }
+}
+
+
---
+++ FocusTimer/TimeLabel.swift
@@ -0,0 +1,16 @@
+//
+//  TimeLabel.swift
+//  FocusTimer
+//
+//  Created by George Higbie on 12/21/20.
+//
+
+import SwiftUI
+
+struct TimeLabel: View {
+    var body: some View {
+        Text("Time Lable")
+    }
+}
+
+}
